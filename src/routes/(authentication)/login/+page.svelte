d6c7dcdf874bdfa142bdb04e662df45ee68561b1
Thomas Mol <email@thomasmol.com>
2023-01-28 17:26:47 +0100

    ui imrpove

--- src/routes/(authentication)/login/+page.svelte
+++ src/routes/(authentication)/login/+page.svelte
@@ -8,7 +8,7 @@
 		<h1 class="text-center text-xl font-semibold text-slate-800">
 			Sign in to use NudgeMe
 		</h1>
-		<p class="mt-3 text-center">Nudges are now saved on your account.</p>
+		<p class="mt-3 text-center">Click <a href="/welcome" class="text-lime-600 text-lg font-semibold underline">here</a>  to try a demo without logging in</p>
 		<p class="mt-4 flex flex-col">
 			<button on:click={() => signIn('github')} class="rounded border bg-slate-800 p-2 text-white"
 				>Sign In with GitHub</button>
